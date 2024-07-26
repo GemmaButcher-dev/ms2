2d957d9328dcc8af302f7da64586c34185365c87
Gemma Butcher <gemmaford1708@icloud.com>
2024-07-26 12:38:42 +0000

    add modal and game shuffling variables

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ let correctAnswer;
 let answersCorrect = 0;
 let timer;
 const TIMER_DURATION = 10;
+
+// Declare game shuffling 
+let shuffledGameData;
+
+// Get the modal to pop up
+let modal = document.getElementById('answerModal');
+let modalMessage = document.getElementById('modal-message');
